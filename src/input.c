da0d630bf681f4153e73850ec14e9b8437dc55b4
Zear <zeartul@gmail.com>
2015-12-19 13:48:54 +0100

    Fix analog joystick

--- src/input.c
+++ src/input.c
@@ -5,12 +5,14 @@
 
 SDL_Event event;
 int keys[2048];
+int joyCanMoveX = 1;
+int joyCanMoveY = 1;
 
 void input()
 {
-	while(SDL_PollEvent(&event))
+	while (SDL_PollEvent(&event))
 	{
-		switch(event.type)
+		switch (event.type)
 		{
 			case SDL_QUIT:
 				quit = 1;
@@ -22,42 +24,60 @@ void input()
 				keys[event.key.keysym.sym] = 0;
 			break;
 			case SDL_JOYAXISMOTION:			// Analog joystick movement
-				switch(event.jaxis.axis)
+				switch (event.jaxis.axis)
 				{
 					case 0:		// axis 0 (left-right)
-						if(event.jaxis.value < -JOY_DEADZONE)
+						if (event.jaxis.value < -JOY_DEADZONE)
 						{
-							// left movement
-							keys[SDLK_LEFT] = 1;
-							keys[SDLK_RIGHT] = 0;
+							if (joyCanMoveX)
+							{
+								joyCanMoveX = 0;
+								// left movement
+								keys[SDLK_LEFT] = 1;
+								keys[SDLK_RIGHT] = 0;
+							}
 						}
-						else if(event.jaxis.value > JOY_DEADZONE)
+						else if (event.jaxis.value > JOY_DEADZONE)
 						{
-							// right movement
-							keys[SDLK_LEFT] = 0;
-							keys[SDLK_RIGHT] = 1;
+							if (joyCanMoveX)
+							{
+								joyCanMoveX = 0;
+								// right movement
+								keys[SDLK_LEFT] = 0;
+								keys[SDLK_RIGHT] = 1;
+							}
 						}
 						else
 						{
+							joyCanMoveX = 1;
 							keys[SDLK_LEFT] = 0;
 							keys[SDLK_RIGHT] = 0;
 						}
 					break;
 					case 1:		// axis 1 (up-down)
-						if(event.jaxis.value < -JOY_DEADZONE)
+						if (event.jaxis.value < -JOY_DEADZONE)
 						{
-							// up movement
-							keys[SDLK_UP] = 1;
-							keys[SDLK_DOWN] = 0;
+							if (joyCanMoveY)
+							{
+								joyCanMoveY = 0;
+								// up movement
+								keys[SDLK_UP] = 1;
+								keys[SDLK_DOWN] = 0;
+							}
 						}
-						else if(event.jaxis.value > JOY_DEADZONE)
+						else if (event.jaxis.value > JOY_DEADZONE)
 						{
-							// down movement
-							keys[SDLK_UP] = 0;
-							keys[SDLK_DOWN] = 1;
+							if (joyCanMoveY)
+							{
+								joyCanMoveY = 0;
+								// down movement
+								keys[SDLK_UP] = 0;
+								keys[SDLK_DOWN] = 1;
+							}
 						}
 						else
 						{
+							joyCanMoveY = 1;
 							keys[SDLK_UP] = 0;
 							keys[SDLK_DOWN] = 0;
 						}
