a0b563f277f4fe56bc8af372777301d20c5a419b
Zear <zeartul@gmail.com>
2015-12-24 13:06:53 +0100

    Upgrade version number to 0.2.0

--- src/main.h
+++ src/main.h
@@ -2,8 +2,8 @@
 #define _MAIN_H_
 
 #define PROGRAM_MAJOR_VERSION 0
-#define PROGRAM_MINOR_VERSION 1
-#define PROGRAM_PATCH_VERSION 1
+#define PROGRAM_MINOR_VERSION 2
+#define PROGRAM_PATCH_VERSION 0
 
 extern int quit;
 
